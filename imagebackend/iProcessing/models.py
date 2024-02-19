0c539b41352baf4420031b7375099f5b42250b4c
Gayathri Nandhana M <98381421+AE-0001@users.noreply.github.com>
2024-02-19 09:38:36 +0530

    Add files via upload

---
+++ imagebackend/iProcessing/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+import hashlib
+import datetime
+import os
+from functools import partial
+
+# Create your models here.
+
+def _update_filename(instance, filename, path):
+    path = path
+    filename = "toProcessImage"+ str(image.objects.count()) + '.jpg'
+    return os.path.join(path, filename)
+
+
+def upload_to(path):
+    return partial(_update_filename, path=path)
+
+
+class image(models.Model):
+   img = models.ImageField(upload_to=upload_to("images"))
+
